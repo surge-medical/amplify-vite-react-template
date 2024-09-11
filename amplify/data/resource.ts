d43755c7ab058d9a7dbaa1d192aefb6b653dedc0
Steve Wang <stevewang@alumni.stanford.edu>
2024-09-11 12:23:00 -0400

    added per-user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,19 +7,17 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
+  Todo: a.model({
       content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
+    }).authorization(allow => [allow.owner()]),
+    });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: "userPool",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
